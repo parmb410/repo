d039262bbffe85d7b0e363a9b92b4fce229490f2
parmb410 <parmb410@gmail.com>
2025-05-31 11:01:55 -0400

    Update util.py

--- diversify/utils/util.py
+++ diversify/utils/util.py
@@ -16,83 +16,138 @@ def set_random_seed(seed=0):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
-class Tee(object):
+def train_valid_target_eval_names(args):
+    eval_name_dict = {'train': [], 'valid': [], 'target': []}
+    for i in range(args.domain_num):
+        if i not in args.test_envs:
+            eval_name_dict['train'].append('eval%d_in' % i)
+            eval_name_dict['valid'].append('eval%d_out' % i)
+        else:
+            eval_name_dict['target'].append('eval%d_out' % i)
+    return eval_name_dict
+
+
+def alg_loss_dict(args):
+    loss_dict = {'diversify': ['class', 'dis', 'total']}
+    return loss_dict[args.algorithm]
+
+
+def print_args(args, print_list):
+    s = "==========================================\n"
+    l = len(print_list)
+    for arg, content in args.__dict__.items():
+        if l == 0 or arg in print_list:
+            s += "{}:{}\n".format(arg, content)
+    return s
+
+
+def print_row(row, colwidth=10, latex=False):
+    if latex:
+        sep = " & "
+        end_ = "\\\\"
+    else:
+        sep = "  "
+        end_ = ""
+
+    def format_val(x):
+        if np.issubdtype(type(x), np.floating):
+            x = "{:.10f}".format(x)
+        return str(x).ljust(colwidth)[:colwidth]
+    print(sep.join([format_val(x) for x in row]), end_)
+
+
+def print_environ():
+    print("Environment:")
+    print("\tPython: {}".format(sys.version.split(" ")[0]))
+    print("\tPyTorch: {}".format(torch.__version__))
+    print("\tTorchvision: {}".format(torchvision.__version__))
+    print("\tCUDA: {}".format(torch.version.cuda))
+    print("\tCUDNN: {}".format(torch.backends.cudnn.version()))
+    print("\tNumPy: {}".format(np.__version__))
+    print("\tPIL: {}".format(PIL.__version__))
+
+
+class Tee:
     def __init__(self, fname, mode="a"):
         self.stdout = sys.stdout
-        self.stderr = sys.stderr
         self.file = open(fname, mode)
 
-    def write(self, data):
-        self.stdout.write(data)
-        self.file.write(data)
+    def write(self, message):
+        self.stdout.write(message)
+        self.file.write(message)
+        self.flush()
 
     def flush(self):
         self.stdout.flush()
         self.file.flush()
 
 
-def print_args(args, print_list):
-    s = "==========================================\n"
-    for name, val in vars(args).items():
-        if (not print_list) or (name in print_list):
-            s += "{}: {}\n".format(name, val)
-    s += "=========================================="
-    return s
-
-
 def act_param_init(args):
-    parser = argparse.ArgumentParser(description="DIVERSIFY Utility Arguments")
-    parser.add_argument('--model', type=str, default="diversify", help="Model name")
-    parser.add_argument('--dataset', type=str, default="ucla", help="Dataset name")
-    parser.add_argument('--data_dir', type=str, default="./data", help="Data directory")
-    parser.add_argument('--data_file', type=str, default="", help="Data file prefix")
-    parser.add_argument('--gpu_id', type=str, default="0", help="CUDA_VISIBLE_DEVICES setting")
-    parser.add_argument('--batch_size', type=int, default=32, help="Mini-batch size")
-    parser.add_argument('--num_epoch', type=int, default=50, help="Number of epochs")
-    parser.add_argument('--lr', type=float, default=1e-3, help="Learning rate")
-    parser.add_argument('--momentum', type=float, default=0.9, help="SGD momentum")
-    parser.add_argument('--num_workers', type=int, default=4, help="Number of data loader workers")
-    parser.add_argument('--feature_dim', type=int, default=128, help="Dimension of feature embeddings")
-    parser.add_argument('--print_freq', type=int, default=10, help="Print frequency during training")
-    parser.add_argument('--eval_freq', type=int, default=1, help="Evaluation frequency (epochs)")
-    parser.add_argument('--save_freq', type=int, default=5, help="Model save frequency (epochs)")
-    parser.add_argument('--old', action='store_true')
+    args.select_position = {'emg': [0]}
+    args.select_channel = {'emg': np.arange(8)}
+    args.hz_list = {'emg': 1000}
+    args.act_people = {'emg': [[i*9+j for j in range(9)]for i in range(4)]}
+    tmp = {'emg': ((8, 1, 200), 6, 10)}
+    args.num_classes, args.input_shape, args.grid_size = tmp[
+        args.dataset][1], tmp[args.dataset][0], tmp[args.dataset][2]
 
+    return args
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='DG')
+    parser.add_argument('--algorithm', type=str, default="diversify")
+    parser.add_argument('--alpha', type=float,
+                        default=0.1, help="DANN dis alpha")
+    parser.add_argument('--alpha1', type=float,
+                        default=0.1, help="DANN dis alpha")
+    parser.add_argument('--batch_size', type=int,
+                        default=32, help="batch_size")
+    parser.add_argument('--beta1', type=float, default=0.5, help="Adam")
+    parser.add_argument('--bottleneck', type=int, default=256)
+    parser.add_argument('--checkpoint_freq', type=int,
+                        default=100, help='Checkpoint every N steps')
+    parser.add_argument('--classifier', type=str,
+                        default="linear", choices=["linear", "wn"])
+    parser.add_argument('--data_file', type=str, default='')
+    parser.add_argument('--dataset', type=str, default='dsads')
+    parser.add_argument('--data_dir', type=str, default='')
+    parser.add_argument('--dis_hidden', type=int, default=256)
+    parser.add_argument('--gpu_id', type=str, nargs='?',
+                        default='0', help="device id to run")
+    parser.add_argument('--layer', type=str, default="bn",
+                        choices=["ori", "bn"])
+    parser.add_argument('--lam', type=float, default=0.0)
+    parser.add_argument('--latent_domain_num', type=int, default=3)
+    parser.add_argument('--local_epoch', type=int,
+                        default=1, help='local iterations')
+    parser.add_argument('--lr', type=float, default=1e-2, help="learning rate")
+    parser.add_argument('--lr_decay1', type=float,
+                        default=1.0, help='for pretrained featurizer')
+    parser.add_argument('--lr_decay2', type=float, default=1.0)
+    parser.add_argument('--max_epoch', type=int,
+                        default=120, help="max iterations")
+    parser.add_argument('--model_size', default='median',
+                        choices=['small', 'median', 'large', 'transformer'])
+    parser.add_argument('--N_WORKERS', type=int, default=4)
+    parser.add_argument('--old', action='store_true')
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--task', type=str, default="cross_people")
     parser.add_argument('--test_envs', type=int, nargs='+', default=[0])
     parser.add_argument('--output', type=str, default="train_output")
     parser.add_argument('--weight_decay', type=float, default=5e-4)
-
-    # ─── Added for Automated K Estimation ──────────────────────────────────
-    parser.add_argument(
-        '--k_min',
-        type=int,
-        default=2,
-        help="Minimum K to try when running automated K estimation"
-    )
-    parser.add_argument(
-        '--k_max',
-        type=int,
-        default=10,
-        help="Maximum K to try when running automated K estimation"
-    )
-    # ──────────────────────────────────────────────────────────────────────
-
+	parser.add_argument('--K', type=int, default=None,help='Number of latent sub-domains; set automatically if None')
     args = parser.parse_args()
     args.steps_per_epoch = 10000000000
-    args.data_dir = args.data_file + args.data_dir
+    args.data_dir = args.data_file+args.data_dir
     os.environ['CUDA_VISIBLE_DEVICS'] = args.gpu_id
     os.makedirs(args.output, exist_ok=True)
     sys.stdout = Tee(os.path.join(args.output, 'out.txt'))
     sys.stderr = Tee(os.path.join(args.output, 'err.txt'))
     args = act_param_init(args)
     return args
-
-
-def get_args():
-    return act_param_init(None)
